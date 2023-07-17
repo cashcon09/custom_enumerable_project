564cc0c792f30faf7ed8746a4854bdbda82babce
Connor Cash <cashconnor0224@gmail.com>
2023-07-17 12:42:03 -0500

    Completed custom enumerables project

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,68 @@
 module Enumerable
   # Your code goes here
+  def my_all?
+    bool = false
+    self.my_each do |item|
+      return false if !yield(item)
+    end
+    true
+  end
+
+  def my_any?
+    self.my_each do |item|
+      return true if yield(item)
+    end
+    false
+  end
+
+  def my_count
+    count = 0
+    return self.length unless block_given?
+
+    self.my_each do |item|
+      count += 1 if yield(item)
+    end
+    count
+  end
+
+  def my_each_with_index
+    index = 0
+    self.my_each do |item|
+      yield(item, index)
+      index += 1
+    end
+  end
+
+  def my_inject(initial_value)
+    count = initial_value
+    self.my_each do |item|
+      count = yield(item, count)
+    end
+    count
+  end
+
+  def my_map
+    arr = []
+    self.my_each do |item|
+      arr.push(yield(item))
+    end
+    arr
+  end
+
+  def my_none?
+    self.my_each do |item|
+      return false if yield(item)
+    end
+    true
+  end
+
+  def my_select
+    arr = []
+    self.my_each do |item|
+      arr.push(item) if yield(item)
+    end
+    arr
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +71,7 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    self.each {|item| yield(item)}
+  end
 end
